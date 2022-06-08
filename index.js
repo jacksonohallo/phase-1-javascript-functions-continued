bf48daa612e886b5127a2d80903a12f8fc50e093
jacksonohallo <jacksonohallo@gmail.com>
2022-06-08 04:14:55 +0300

    commit tree 04e39270075d72fb423fa18ffe90302b0cc4ac19

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 function saturdayFun(activity="roller-skate") {
   return `This Saturday, I want to ${activity}!`
 }
-
-let mondayWork = function(activity="go to the office") {
-  return `This Monday, I will ${activity}.`
+let mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
 }
 
 let wrapAdjective = function(style="*") {
